18596011c36107566a6c25d42f0d623e35af2b61
Yu Guo <yuguo01462@gmail.com>
2015-10-27 16:04:11 -0700

    add CSS and additional html component
    add year and director of movie to data structure
    
    add minor styling changes

--- movie_trailer/fresh_tomatos.py
+++ movie_trailer/fresh_tomatos.py
@@ -124,7 +124,8 @@ main_page_content = '''
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2>
+    <h2>{movie_title} ({year})</h2>
+    <span class="text-muted">Directed by: <strong>{director}</strong></span>
 </div>
 '''
 
@@ -145,7 +146,9 @@ def create_movie_tiles_content(movies):
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
+            year=movie.year,
+            director=movie.director
         )
     return content
 
--- movie_trailer/movies_data.py
+++ movie_trailer/movies_data.py
@@ -8,10 +8,12 @@ class Movie:
     The Movie class stores relevant properties of a movie, such as
     its title, box art url, youtube trailer url, etc.
     """
-    def __init__(self, title, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, poster_image_url, trailer_youtube_url, year, director):
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
+        self.year = year
+        self.director = director
 
 
 def load_movies():
@@ -21,17 +23,23 @@ def load_movies():
     minions = Movie(
         'Minions',
         'https://upload.wikimedia.org/wikipedia/en/3/3d/Minions_poster.jpg',
-        'https://www.youtube.com/watch?v=P9-FCC6I7u0')
+        'https://www.youtube.com/watch?v=P9-FCC6I7u0',
+        2015,
+        'Pierre Coffin, Kyle Balda')
 
     spirit = Movie(
         'Spirit: Stallion of the Cimarron',
         'https://upload.wikimedia.org/wikipedia/en/3/3b/Spirit_Stallion_of_the_Cimarron_poster.jpg',
-        'https://www.youtube.com/watch?v=8Nj4_L3Vbu0')
+        'https://www.youtube.com/watch?v=8Nj4_L3Vbu0',
+        2002,
+        'Kelly Asbury, Lorna Cook')
 
     madagascar = Movie(
         'Madagascar',
         'https://upload.wikimedia.org/wikipedia/en/3/36/Madagascar_Theatrical_Poster.jpg',
-        'https://www.youtube.com/watch?v=hdcTmpvDO0I')
+        'https://www.youtube.com/watch?v=hdcTmpvDO0I',
+        2005,
+        'Tom McGrath, Eric Darnell')
 
     return [minions, spirit, madagascar]
 
