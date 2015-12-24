2ecb9df33bc12fe6e9c47fd6d7d70342400e3541
Yu Guo <yuguo01462@gmail.com>
2015-12-24 12:30:26 -0800

    change host and port in application

--- linux-server/catalog/application.py
+++ linux-server/catalog/application.py
@@ -390,4 +390,4 @@ def deleteItemByName(category_name, item_name):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5432)
+    app.run(host = '127.0.0.1', port = 5050)
